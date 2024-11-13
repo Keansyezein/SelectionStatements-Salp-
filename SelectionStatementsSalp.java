7b58409558014a782361453e8db62f6733dd3475
Keansyezein <148431554+Keansyezein@users.noreply.github.com>
2024-11-13 09:53:37 +0800

    Add files via upload

---
+++ SelectionStatementsSalp.java
@@ -0,0 +1,18 @@
+package selectionstatements.salp;
+
+public class SelectionStatementsSalp {
+    public static void main(String[] args) {
+       int iNumber = 3;
+       
+       if(iNumber >= 0){
+            if(iNumber == 0){
+                System.out.println("First String.");
+            } else{
+                System.out.println("Second String");
+            }   
+       }
+       
+        System.out.print("Third String");
+    }
+    
+}
